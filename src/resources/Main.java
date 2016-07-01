0995c9203b91b477289549ee9faf195a6c9e6c1c
Maxwell <maxwell.acioli@gmail.com>
2016-07-01 10:37:37 -0300

    Update Main.java

--- src/resources/Main.java
+++ src/resources/Main.java
@@ -8,42 +8,40 @@ public class Main {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 
+		
 		Scanner input = new Scanner(System.in);
 		int capacity = input.nextInt();
 
-		if (capacity < 0) {
-			throw new Exception();
-		}
-
 		queue = new Queue(capacity);
-
-		while (input.hasNextInt()) {
-			int number = input.nextInt();
-
-			queue.addElement(number);
-		}
 
 		String cmd = input.next();
 
 		while (!cmd.equals("quit")) {
-			command(cmd, input);
+			try {
+				command(cmd, input);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			cmd = input.next();
 		}
+
+		input.close();
 
 		input.close();
 	}
 
 	private static void command(String cmd, Scanner input) throws Exception {
-		if (cmd.equals("element")) {
+	if (cmd.equals("element")) {
 			System.out.println(queue.getHead());
-		} else if (cmd.equals("size")) {
+		} else if (cmd.equals("size")) {	
 			System.out.println(queue.getSize());
 		} else if (cmd.equals("print")) {
 			queue.printQueue();
 		} else if (cmd.equals("remove")) {
 			queue.removeNumber();
-		} else if (cmd.equals("insert")) {
-			while (input.hasNextInt()) {
+		} else if (cmd.equals("add")) {
+			if (input.hasNextInt()) {
 				queue.addElement(input.nextInt());
 			}
 		} else {
