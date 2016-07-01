98f034b6a8aa9ddf43f3d35f63299a0c443d5299
Maxwell <maxwell.acioli@gmail.com>
2016-07-01 10:38:32 -0300

    Update Queue.java

--- src/resources/Queue.java
+++ src/resources/Queue.java
@@ -1,43 +1,45 @@
 package resources;
 
 public class Queue {
-	private int[] queue;
+private int[] queue;
 	private int max_size;
 	private int size = 0;
 	private final int head = 0;
-	
+
 	public Queue(int max_size) {
 		this.max_size = max_size;
 		queue = new int[max_size];
 	}
-	
+
 	public void setMaxSize(int max_size) {
 		this.max_size = max_size;
 		queue = new int[max_size];
 	}
 
-	public void addElement(int number) throws Exception {
-		if(size >= max_size) {
-			throw new Exception();
+	public void addElement(int number) {
+		if (size >= max_size) {
+			System.exit(1);
 		}
-		
+
 		queue[size++] = number;
 	}
 
-	public int removeNumber() throws Exception {
-		if(size <= 0) {
-			throw new Exception();
-		}
-		
-		int numberRemoved = queue[head];
-		int i;
-		for(i = 0; i < size - 1 ; i++) {
-			queue[i] = queue[i+1]; 
-		}
-		
-		size--;
+	public int removeNumber() {
 
-		return numberRemoved;
+		if (size > 0) {
+			int numberRemoved = queue[head];
+			int i;
+			for (i = 0; i < size - 1; i++) {
+				queue[i] = queue[i + 1];
+			}
+
+			size--;
+
+			return numberRemoved;
+		} else {
+			System.out.println("Nao foi possivel remover");
+			return -1;
+		}
 	}
 
 	public int getHead() {
@@ -46,15 +48,13 @@ public class Queue {
 	}
 
 	public int getSize() {
-		
+
 		return size;
 	}
-	
+
 	public void printQueue() {
-		int i;
-		
-		for(i = 0 ; i < size ; i++) {
-			System.out.println(queue[i]);
+		while (size > 0) {
+			System.out.println(removeNumber());
 		}
 	}
 }
